6d153efb1181f13c2df7917a4d828edcafbab5cb
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-03-03 21:20:44 -0300

    Adicionar arquivos de projeto.

---
+++ ComboBox CSharp/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace ComboBox_CSharp
+{
+    partial class ComboBox
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ComboBox));
+            this.SuspendLayout();
+            // 
+            // ComboBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(877, 478);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "ComboBox";
+            this.Text = "ComboBox";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
